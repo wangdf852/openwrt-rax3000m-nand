03f0a50168b88d7c8e897dea971f5f1d48bc6810
wangdf852 <79922028+wangdf852@users.noreply.github.com>
2026-04-06 02:56:16 +0800

    更新 diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -5,34 +5,27 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-# 说明：
-#
-# 除了第一行的#!/bin/bash不要动，其他的设置，前面带#表示不起作用，不带的表示起作用了（根据你自己需要打开或者关闭）
-#
-# 通过修改diy-part2.sh文件可以自定义默认IP，登陆密码等。按我的需要现在的默认IP为172.16.1.1
-# 修改openwrt登陆地址,把下面的172.16.1.1修改成你想要的就可以了
-sed -i 's/192.168.1.1/172.16.10.1/g' ./package/base-files/files/bin/config_generate
 
-# 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能纯数字或者使用中文）
-sed -i 's/OpenWrt/immortalwrt/g' ./package/base-files/files/bin/config_generate
+# 修改后台IP为 192.168.10.1
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
-# Enable wifi
-# sed -i 's/.disabled=1/.disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# 修改主机名
+sed -i 's/OpenWrt/NbWrt/g' package/base-files/files/bin/config_generate
 
-# echo "修改wifi名称"
-# sed -i "s/OpenWrt/$wifi_name/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# 启用WiFi
+sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
-# Set Wifi SSID and Password
-# sed -i 's/.ssid=OpenWrt/.ssid=Tomato24/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-# sed -i 's/.encryption=none/.encryption=psk-mixed/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-# sed -i '/set\ wireless.default_radio${devidx}.encryption=psk-mixed/a set\ wireless.default_radio${devidx}.key=Psn@2416' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# 设置2.4G WiFi名称
+sed -i 's/ssid=OpenWrt/ssid=NbWrt-888/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
-# 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
-# sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
+# 设置5G WiFi名称
+sed -i '/set wireless.default_radio1/a set wireless.default_radio1.ssid=NbWrt-888-5G' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
-# Set default root password
-# sed -i 's/root::0:0:99999:7:::/root:$1$kWRCl0Y2$7JL\/jLAF1xoVIiIMdTO5f.:16788:0:99999:7:::/g' package/base-files/files/etc/shadow
+# 设置WiFi加密方式为WPA2
+sed -i 's/encryption=none/encryption=psk2/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
-# 修改默认主题Modify default THEME
-# sed -i 's/luci-theme-bootstrap/luci-theme-atmaterial_new/g' ./feeds/luci/collections/luci/Makefile
+# 设置WiFi统一密码 123321123
+sed -i '/encryption=psk2/a set wireless.default_radio${devidx}.key=123321123' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
+# 设置默认主题为 luci-theme-argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
